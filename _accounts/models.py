e510b55654b0b214f15485f430b44051c85e52de
RimPeter <hull@localappliancerentals.co.uk>
2024-10-24 00:20:17 +0100

    remove password_hash from User model

--- _accounts/models.py
+++ _accounts/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class User(models.Model):
     email = models.EmailField(unique=True)
-    password_hash = models.CharField(max_length=255)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=15)
