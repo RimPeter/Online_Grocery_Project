4eb0b290fda0900ea0d753a187ca7dc5c7004345
RimPeter <hull@localappliancerentals.co.uk>
2024-12-04 18:21:26 +0000

    Add product list html

--- _catalog/views.py
+++ _catalog/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, '_catalog/product.html', context)
 
 def home(request):
     return render(request, '_catalog/home.html')
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, '_catalog/product_detail.html', context)
