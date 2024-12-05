8dee5fbb115ea6b8eba983cf1e2f3859088fdbee
RimPeter <hull@localappliancerentals.co.uk>
2024-12-05 22:35:59 +0000

    Display images in product list on admin site

--- _catalog/admin.py
+++ _catalog/admin.py
@@ -25,14 +25,13 @@ from django.utils.html import format_html
         
 @admin.register(All_Products)
 class All_ProductsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'url', 'image_url', 'price')
+    list_display = ('name', 'url', 'image_link', 'price')
     list_filter = ('name', 'price')
     search_fields = ('name', 'sku')
     ordering = ('name',)
-    
-    @admin.display(description='Image_url')
+
+    @admin.display(description='Image')
     def image_link(self, obj):
-        if obj.image:
-            return format_html('<img src="{}" width="50" height="50" />', obj.image)
-        else:
-            return 'No image'
+        if obj.image_url:
+            return format_html('<img src="{}" width="50" height="50" />', obj.image_url)
+        return 'No image'
