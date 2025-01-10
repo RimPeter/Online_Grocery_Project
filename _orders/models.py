f2cf35b43acf191cea94324b63f880566982c327
RimPeter <hull@localappliancerentals.co.uk>
2025-01-10 18:06:57 +0000

    Fix OrderItem model in _orders\models.py

--- _orders/models.py
+++ _orders/models.py
@@ -34,4 +34,4 @@ class OrderItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"Order #{self.order_id} | {self.product.product_name} x {self.quantity}"
+        return f"Order #{self.order_id} | {self.product.name} x {self.quantity}"
