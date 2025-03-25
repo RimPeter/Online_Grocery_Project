2128df68247c206ff63eeeae3a6fe90783618e67
RimPeter <hull@localappliancerentals.co.uk>
2025-03-25 06:55:12 +0000

    Enhance order_summery.html and admin
    1. remove 'Proceed to Pay' option
    2. show image of item in order summery
    3. create order summary in admin for all orders, so we know what to order
    4. create filtering options in admin for orders

--- _orders/admin.py
+++ _orders/admin.py
@@ -9,12 +9,28 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'total', 'created_at')
-    list_filter = ('user', 'created_at')
+    list_display = ('id', 'user', 'status', 'total', 'created_at')
+    list_editable = ('status',)
+    list_filter = ('user', 'created_at', 'status')
+    action = ['mark_as_delivered']
     search_fields = ('user__username', 'user__email')
+    
+    def mark_as_delivered(self, request, queryset):
+        updated = queryset.update(status='delivered')
+        self.message_user(request, f"{updated} orders marked as delivered.")
+    mark_as_delivered.short_description = "Mark selected orders as delivered"
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'order', 'product', 'quantity', 'price')
-    list_filter = ('product',)
+    list_display = ('supplier_completed', 'order', 'product', 'quantity', 'price')
+    list_filter = ('supplier_completed', 'order__status',)
+    actions = ['mark_as_completed']
     search_fields = ('order__id', 'product__product_name')
+    
+    def mark_as_completed(self, request, queryset):
+        queryset.update(supplier_completed=True)
+        self.message_user(request, "Selected items marked as completed.")
+    mark_as_completed.short_description = "Mark selected items as completed"
+    
+
+
--- _orders/models.py
+++ _orders/models.py
@@ -5,8 +5,8 @@ from _catalog.models import All_Products
 class Order(models.Model):
     STATUS_CHOICES = [
         ('pending', 'Pending'),
-        ('processing', 'Processing'),
         ('paid', 'Paid'),
+        ('processed', 'Processed'),
         ('delivered', 'Delivered'),
         ('canceled', 'Canceled'),
     ]   
@@ -35,6 +35,11 @@ class OrderItem(models.Model):
     product = models.ForeignKey(All_Products, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    supplier_completed = models.BooleanField(
+        default=False,
+        help_text="Mark if we've already ordered this item from suppliers."
+    )
 
     def __str__(self):
         return f"Order #{self.order_id} | {self.product.name} x {self.quantity}"
