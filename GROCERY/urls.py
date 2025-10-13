da10e0dcd4332fc6e41016eed7df6c6f4b4c909e
RimPeter <hull@localappliancerentals.co.uk>
2025-10-13 04:23:45 +0100

    Add favicon

--- GROCERY/urls.py
+++ GROCERY/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.templatetags.static import static as static_static
 from _accounts.views import custom_404_view
 
 
@@ -9,6 +11,8 @@ urlpatterns = [
     path('accounts/', include('_accounts.urls')),
     path('payments/', include('_payments.urls')),
     path('orders/', include('_orders.urls')),
+    # Redirect browsers that request /favicon.ico to the static SVG
+    path('favicon.ico', RedirectView.as_view(url=static_static('images/favicon.svg'), permanent=True)),
     
 ]
 
